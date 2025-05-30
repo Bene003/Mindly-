4eaeb464f91f55a9b0fe39dbd721e06b3b039105
Bene003 <kweteeben@gmail.com>
2025-05-29 22:55:58 -0400

    Dashboard pt-1

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,46 @@
-import React from 'react'
-import { Button } from "@/components/ui/button"
+import CompanionCard from '@/components/CompanionCard'
+import CompanionList from '@/components/CompanionList'
+import CTA from '@/components/CTA'
 
 
 const page = () => {
   return (
-    <div>
-      <h1 className="text-2xl underline">Welcome to Mindly</h1>
-      <Button>
-        Let's go 
-      </Button>
-    </div>
+    <main>
+      <h1>Assistant Populaire</h1>
+        <section className='home-section'>
+            <CompanionCard
+                id="123"
+                name="Ceuvra, l’exploratrice du cerveau"
+                topic="Réseaux neuronaux du cerveau"
+                subject="science"
+                duration={45}
+                color="#FCCC41"                
+                
+            />
+            <CompanionCard
+                id="456"
+                name="Compta, la magicienne des nombres"
+                topic="Dérivées et intégrales"
+                subject="Maths"
+                duration={30}
+                color="#C8E6C9"
+            />
+            <CompanionCard
+                id="789"
+                name="Vocala, la bâtisseuse de vocabulaire"
+                topic="Littérature anglaise"
+                subject="Langue"
+                duration={30}
+                color="#BDE7FF"
+            />
+        </section>
+
+        <section className='home-section'>
+            <CompanionList />
+            <CTA />
+        </section>
+
+    </main>
   )
 }
 
